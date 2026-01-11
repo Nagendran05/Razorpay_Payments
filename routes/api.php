1e1475f27bd11f5ce048865a475df1200b56faa5
Nagendran05 <nagendranp.it@gmail.com>
2026-01-11 21:56:47 +0530

    Razorpay_api app starting

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PaymentController;
+
+Route::post('/create-order',[PaymentController::class,'createorder']);
+Route::post('verify-payments',[PaymentController::class,'verifypayments']);
