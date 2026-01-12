e537e19adc3126a12307d92e07716aa7cf9beee1
Nagendran <nagendran.p@uzhavarbumi.com>
2026-01-12 16:22:32 +0530

    [FEATURE]StoreDataInDB

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PaymentController;
 
 Route::post('/create-order',[PaymentController::class,'createorder']);
-Route::post('verify-payments',[PaymentController::class,'verifypayments']);
+Route::post('/verify-payments',[PaymentController::class,'verifypayments']);
+Route::post('/razorpay-webhook',[PaymentController::class,'webhook']);
